76383151e08602750c932e8a9020ceda4350e39a
Krittika Chaturvedi <krittika.chaturvedi2019@vitstudent.ac.in>
2020-12-12 20:54:56 +0530

    Added files

---
+++ app/src/main/java/com/krittika/diceroller/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.krittika.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.krittika.diceroller.R.*
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+        val rollButton: Button = findViewById(id.roll_button)
+        rollButton.setOnClickListener { rollDice()
+        }
+    }
+    private fun rollDice(){
+        val random= Random().nextInt(6) + 1
+        val drawableResource = when (random) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        val diceImage: ImageView = findViewById(id.dice_image)
+        diceImage.setImageResource(drawableResource)
+
+
+
+    }
+}
+
+
+
+
